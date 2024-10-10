496c77875b6f91ed6798d623b57711e93450b71f
501Legion <gunner07050@gmail.com>
2024-10-10 15:25:20 +0900

    통합해서 올림

---
+++ lecture/ch09_import.py
@@ -0,0 +1,14 @@
+# ** import
+# 1. import 라이브러리
+# ex) import requests
+# -> requests 라이브러리의 모든 기능을 불러오기
+# -> 사용법 : requests.
+
+# 2. from 라이브러리 import 모듈
+# ex) from requests import get, post
+#   -> get, post 모듈만 불러오기
+#   -> 사용법: get(), post()
+#  * as(Alias: 별명)
+#  ex) import BeautifulSoup as bs4
+#   -> BeautifulSoup을 bs4로 부르자!
+#   -> 사용법: bs4.select()
